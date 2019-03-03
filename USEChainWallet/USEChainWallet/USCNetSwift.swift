cbccdb6140fb0bfb7fb47440778374d11f29497c
Jacob <370694805@163.com>
2019-03-03 11:47:01 +0800

    bridging file

---
+++ USEChainWallet/USEChainWallet/USCNetSwift.swift
@@ -0,0 +1,13 @@
+//
+//  USCNetSwift.swift
+//  USEChainWallet
+//
+//  Created by Jacob on 2019/3/3.
+//  Copyright © 2019 Jacob. All rights reserved.
+//
+
+import UIKit
+
+class USCNetSwift: NSObject {
+
+}
